108a99a82522b77c63e2bca94999b77e06093210
Jamie Tomlinson <jamietomlinson312@gmail.com>
2026-07-23 10:32:02 +0100

    Added folder for practicing JavaScript

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("button");
+
+// Add EventListener to btn
+btn.addEventListener("click", function () {
+    const para = document.getElementById("demo");
+    console.log = para.innerHTML
+});
