2b8ee06e10b0b1a84de9c4d31f66f187d14f737a
Jamie Tomlinson <jamietomlinson312@gmail.com>
2026-08-19 17:47:15 +0100

    Added code to practice using template literals and querySelectorAll method

--- JavaScript/Activity - basic banking system/bankAccount.js
+++ JavaScript/Activity - basic banking system/bankAccount.js
@@ -1,20 +1,22 @@
 export class BankAccount {
-    constructor (accountNumber, accountHolder, balance) { // Initialise BankAccount Class
+    constructor (accountNumber, accountHolder, balance, password) { // Initialise BankAccount Class
         this.accountNumber = accountNumber;
         this.accountHolder = accountHolder;
         this.balance = balance;
+        this.password = password;
     }
 
     // Methods for depositing, withdrawing and checking balance
 
     deposit(depositAmount) {
-        this.balance += depositAmount; // Updates the balance property
-        console.log(depositAmount + ' was deposited')
-        return this.balance
+            this.balance += depositAmount; // Updates the balance property
+            console.log(depositAmount + ' was deposited');
+            return this.balance;
     }
     withdraw(withdrawAmount) {
         if (withdrawAmount > this.balance) {
             alert("Insufficient funds");
+            return this.balance // return balance before attempted withdrawal
         } else {
             console.log(withdrawAmount + ' was withdrawn')
             return this.balance -= withdrawAmount;
--- JavaScript/Activity - basic banking system/index.js
+++ JavaScript/Activity - basic banking system/index.js
@@ -1,43 +1,62 @@
-import { BankAccount } from "./bankAccount.js"; // Importing the BankAccount class 
+import { BankAccount } from "./bankAccount.js"; // Importing the BankAccount class from external .js file
 
-// Creating a 2 instances of the BankAccountclass with a random account number from 0-100
-
-const newAccount = new BankAccount((Math.floor((Math.random() * 100) + 1)), "John", 1000); 
+const newAccount = new BankAccount((Math.floor((Math.random() * 100) + 1)), "Dave", 1000, "piano"); // creating BankAccount instance
 console.log("Account created for:" , newAccount.accountHolder, newAccount);
-const newAccount2 = new BankAccount((Math.floor((Math.random() * 100) + 1)), "Dave", 15000);
-console.log("Account created for:" , newAccount2.accountHolder, newAccount2);
+document.getElementById("number").innerHTML = `Account Number: ${newAccount.accountNumber}`; // populating data fields with properties
+document.getElementById("holder").textContent = `Account Holder: ${newAccount.accountHolder}`; 
+document.getElementById("balance").textContent = `Account Balance: ${newAccount.balance}`; 
+let summaryHTML = `Hello ${newAccount.accountHolder}, your account number is ${newAccount.accountNumber} and your initial balance is 
+${newAccount.balance}`; // using template literals to render summary text
+document.getElementById("summary").innerHTML = summaryHTML;
 
-//Populating the data fields with the newAccount properties
-
-document.getElementById("number").innerHTML = newAccount.accountNumber; 
-document.getElementById("holder").textContent = newAccount.accountHolder;
-document.getElementById("balance").textContent = newAccount.balance;
 
 //Adding event listeners to buttons
 
-const depo = document.getElementById("b_deposit") // HTML button elements
-const withdraw = document.getElementById("b_withdraw")
-const bal = document.getElementById("b_balance")
+const depo = document.getElementById("b_deposit"); // HTML button elements
+const withdraw = document.getElementById("b_withdraw");
+const bal = document.getElementById("b_balance");
+const col = document.getElementById("b_colour");
 
 depo.addEventListener("click", function () {
-    let depositValue = Number(prompt("Enter amount to deposit:")); // Prompt user for a value and convert to a Number using Number()
-    newAccount.deposit(depositValue); // Call the deposit(x) function
-    document.getElementById("balance").innerHTML = newAccount.balance; //update the "balance" HTML element
-    if (!(document.getElementById("new_balance").innerHTML === "")){ // Updates lower balance message only if not empty
-        document.getElementById("new_balance").innerHTML = "Current balance is: " + newAccount.balance;
-    };
+let depositValue = Number(prompt("Enter amount to deposit:")); // Prompt user for a value and convert to a Number using Number()
+newAccount.deposit(depositValue); // Call the deposit(x) function
+document.getElementById("balance").innerHTML = newAccount.balance; //update the "balance" HTML element
+if (!(document.getElementById("new_balance").innerHTML === "")){ // Updates lower balance message only if not empty
+    document.getElementById("new_balance").innerHTML = "Current balance is: " + newAccount.balance;
+};
+document.getElementById("balance").textContent = `Account Balance: ${newAccount.balance}`; // Update balance field
+updateSummary();
 });
+
 withdraw.addEventListener("click", function () {
-    let withdrawValue = Number(prompt("Enter amount to withdraw:"));
-    newAccount.withdraw(withdrawValue);
-    document.getElementById("balance").innerHTML = newAccount.balance;
-    if (!(document.getElementById("new_balance").innerHTML === "")){
-        document.getElementById("new_balance").innerHTML = "Current balance is: " + newAccount.balance;
-    };
+let withdrawValue = Number(prompt("Enter amount to withdraw:"));
+newAccount.withdraw(withdrawValue);
+document.getElementById("balance").innerHTML = newAccount.balance;
+if (!(document.getElementById("new_balance").innerHTML === "")){
+    document.getElementById("new_balance").innerHTML = "Current balance is: " + newAccount.balance;
+};
+document.getElementById("balance").textContent = `Account Balance: ${newAccount.balance}`; // Update balance field
+updateSummary();
 });
 
 bal.addEventListener("click", function () {
-    let currentBalance = newAccount.checkBalance()
-    document.getElementById("new_balance").innerHTML = "Current balance is: " + newAccount.balance;
+newAccount.checkBalance() // call function
+document.getElementById("new_balance").innerHTML = "Current balance is: " + newAccount.balance;
+document.getElementById("new_balance").style.border = "2px solid green";
 });
+
+col.addEventListener("click", function() {
+const newColour = document.querySelectorAll("p");
+for (let i = 0; i < newColour.length; i++) {
+    newColour[i].style.color = "blue";
+    }
+});
+
+//function for updating the 'summary' text
+
+function updateSummary(){
+    summaryHTML = `Hello ${newAccount.accountHolder}, your account number is ${newAccount.accountNumber} and your current balance is 
+    ${newAccount.balance}`; // this will contain the updated newAccount properties, after the deposit or withdraw function is called
+    document.getElementById("summary").innerHTML = summaryHTML; // reassigning the HTML content 
+};
 
